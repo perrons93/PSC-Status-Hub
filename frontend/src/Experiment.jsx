41da87d27f2a8cb0824b41839b041c1c17169ea4
Caley Brock <caleybrock7@gmail.com>
2019-01-28 13:33:32 -0500

    Make language and look consistent on CAT

--- frontend/src/Experiment.jsx
+++ frontend/src/Experiment.jsx
@@ -1,12 +1,11 @@
 /* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
-import './App.css';
 
 class Experiment extends Component {
   render() {
     return (
       <div>
-        <h2>Experiment</h2>
+        <h2>Prototypes</h2>
         <p>
           This page will be used to test out experimental UIs.
         </p>
