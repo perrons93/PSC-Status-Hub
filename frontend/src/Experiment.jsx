ffa485195ec0c1adc6534fc33e9c9491355e6b35
Caley Brock <caleybrock7@gmail.com>
2019-01-29 08:19:16 -0500

    Update typo

--- frontend/src/Experiment.jsx
+++ frontend/src/Experiment.jsx
@@ -5,7 +5,7 @@ class Experiment extends Component {
   render() {
     return (
       <div>
-        <h2>Prototypes</h2>
+        <h2>Prototype</h2>
         <p>
           This page will be used to test out experimental UIs.
         </p>
