cbf90e2928a03a46d7de8d7efeb5f2abe5f68c8e
Michael Cherry <zorackiii@gmail.com>
2019-02-07 10:54:44 -0500

    Added second 'page' and next button

--- frontend/src/components/eMIB/eMIB.jsx
+++ frontend/src/components/eMIB/eMIB.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 
-const eMIB = () => {
-  return (
-    <div>
-      <h2>eMIB Sample</h2>
-    </div>
-  );
-};
+class eMIB extends Component {
+  state = {
+    curPage: 1
+  };
+
+  changePage = () => {
+    this.setState({ curPage: 2 });
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>eMIB Sample</h2>
+        {this.state.curPage === 1 && <p>#1 Welcome to the eMIB Sample Test</p>}
+        {this.state.curPage === 2 && <p>#2 Test Instructions</p>}
+        <a onClick={this.changePage}>
+          <div>Next</div>
+        </a>
+      </div>
+    );
+  }
+}
 
 export default eMIB;
