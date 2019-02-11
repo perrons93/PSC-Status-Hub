954b40408e04573d08ccb4b18ee2bab97213c395
Michael Cherry <zorackiii@gmail.com>
2019-02-11 09:35:06 -0500

    Updated test to reflect previous change

--- frontend/src/test/Tabs.test.js
+++ frontend/src/test/Tabs.test.js
@@ -4,12 +4,12 @@ import Tab, { SELECTED, UNSELECTED } from "../components/eMIB/Tab";
 
 it("renders selected tab", () => {
   const wrapper = shallow(<Tab tabName="Tab1" selected={SELECTED} />);
-  const initialMessage = <span style={{ color: "black" }}>Tab1</span>;
+  const initialMessage = <span style={{ color: "black", cursor: "pointer" }}>Tab1</span>;
   expect(wrapper.contains(initialMessage)).toEqual(true);
 });
 
 it("renders unselected tab", () => {
   const wrapper = shallow(<Tab tabName="Tab2" selected={UNSELECTED} />);
-  const initialMessage = <span style={{ color: "blue" }}>Tab2</span>;
+  const initialMessage = <span style={{ color: "blue", cursor: "pointer" }}>Tab2</span>;
   expect(wrapper.contains(initialMessage)).toEqual(true);
 });
