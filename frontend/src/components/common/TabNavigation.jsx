57190f680119bc3c3832d1017808debd1814c417
Michael Cherry <zorackiii@gmail.com>
2019-02-12 15:01:24 -0500

    Adding 'body' to the tab so that the new component loads when tabs are changed

--- frontend/src/components/common/TabNavigation.jsx
+++ frontend/src/components/common/TabNavigation.jsx
@@ -5,30 +5,35 @@ import Tab from "./Tab";
 class TabNavigation extends Component {
   static propTypes = {
     tabSpecs: PropTypes.array.isRequired,
-    selected: PropTypes.number.isRequired
+    curTab: PropTypes.number.isRequired
   };
 
   state = {
     tabSpecs: this.props.tabSpecs,
-    selected: this.props.selected
+    curTab: this.props.curTab,
+    curBody: this.props.tabSpecs[this.props.curTab].body
   };
 
   selectTab(id) {
     let tempSpecs = this.state.tabSpecs;
-    tempSpecs[this.state.selected].selected = "false";
+    tempSpecs[this.state.curTab].selected = "false";
     tempSpecs[id].selected = "true";
-    this.setState({ selected: id, tabSpecs: tempSpecs });
+
+    this.setState({ curTab: id, tabSpecs: tempSpecs, curBody: tempSpecs[id].body });
   }
 
   render() {
     return (
       <div>
-        {this.state.tabSpecs.map((tab, key) => (
-          <span key={tab.id} onClick={() => this.selectTab(tab.id)}>
-            <Tab tabName={tab.tabName} selected={tab.selected} />
-            &nbsp;&nbsp;&nbsp;&nbsp;
-          </span>
-        ))}
+        <div>
+          {this.state.tabSpecs.map((tab, key) => (
+            <span key={tab.id} onClick={() => this.selectTab(tab.id)}>
+              <Tab tabName={tab.tabName} selected={tab.selected} />
+              &nbsp;&nbsp;&nbsp;&nbsp;
+            </span>
+          ))}
+        </div>
+        <div>{this.state.curBody}</div>
       </div>
     );
   }
