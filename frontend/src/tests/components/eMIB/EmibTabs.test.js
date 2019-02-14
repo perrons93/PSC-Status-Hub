47d6e2be9c77d056afbee9f4c0643539282afede
Michael Cherry <zorackiii@gmail.com>
2019-02-14 07:49:43 -0500

    Moved Tab and TabNavigation tests to commons folder; fixed bug with EmibTabsTest

--- frontend/src/tests/components/eMIB/EmibTabs.test.js
+++ frontend/src/tests/components/eMIB/EmibTabs.test.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { mount } from "enzyme";
 import EmibTabs from "../../../components/eMIB/EmibTabs";
-import { STRINGS } from "../../../components/eMIB/Emib";
+import LOCALIZE from "../../../text_resources";
 
 it("renders emibTabs page", () => {
   const wrapper = mount(<EmibTabs />);
-  const backgroundMessage = <h2>{STRINGS.backgroundPageTitle}</h2>;
-  expect(wrapper.contains(backgroundMessage)).toEqual(true);
+  const pageTitle = <h2>{LOCALIZE.emibTest.backgroundPage.title}</h2>;
+  expect(wrapper.contains(pageTitle)).toEqual(true);
 });
