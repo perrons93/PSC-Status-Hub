1d3e2ccde666d9d5f3782f741e32c9d04f0666d6
Francis Normand <francis.normand@canada.ca>
2019-02-15 10:13:16 -0500

    Made the selected tab working + created new PATH const instead of hardcoded ones

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 import "./css/lib/aurora.min.css";
 import "./css/App.css";
 import Status from "./Status";
@@ -9,6 +9,31 @@ import Emib from "./components/eMIB/Emib";
 import Translation, { LANGUAGES } from "./components/commons/Translation";
 import LOCALIZE from "./text_resources";
 import psc_header from "./psc_header.png";
+
+const PATH = {
+  home: "/",
+  experiment: "/experiment",
+  status: "/status",
+  emibSampleTest: "/emib-sample"
+};
+
+const isHomeActive = (match, location) => {
+  if (!location) return false;
+  const { pathname } = location;
+  return pathname === PATH.home;
+};
+
+const isExperimentActive = (match, location) => {
+  if (!location) return false;
+  const { pathname } = location;
+  return pathname === PATH.experiment;
+};
+
+const isStatusActive = (match, location) => {
+  if (!location) return false;
+  const { pathname } = location;
+  return pathname === PATH.status;
+};
 
 class App extends Component {
   state = {
@@ -33,22 +58,37 @@ class App extends Component {
             <div id="psc_image" className="pscHeader">
               <img src={psc_header} alt="psc_header" />
             </div>
-            <div className="fixed-top mx-auto nav-site scroll nav nav-tabs">
-              <ul id="tabs" className="mx-auto nav-site scroll nav nav-tabs">
-                <li className="nav-item bg-white">
-                  <Link className="nav-link active-header-tab" to="/">
+            <div className="fixed-top nav nav-tabs">
+              <ul className="mx-auto nav-site nav nav-tabs nav-item">
+                <li className="bg-white">
+                  <NavLink
+                    isActive={isHomeActive}
+                    className="nav-link"
+                    activeClassName="selectedTab"
+                    to="/"
+                  >
                     {LOCALIZE.mainTabs.homeTabTitle}
-                  </Link>
+                  </NavLink>
                 </li>
-                <li className="nav-item bg-white">
-                  <Link className="nav-link active-header-tab" to="/experiment">
+                <li className="bg-white">
+                  <NavLink
+                    isActive={isExperimentActive}
+                    className="nav-link"
+                    activeClassName="selectedTab"
+                    to="/experiment"
+                  >
                     {LOCALIZE.mainTabs.prototypeTabTitle}
-                  </Link>
+                  </NavLink>
                 </li>
-                <li className="nav-item bg-white">
-                  <Link className="nav-link active-header-tab" to="/status">
+                <li className="bg-white">
+                  <NavLink
+                    isActive={isStatusActive}
+                    className="nav-link"
+                    activeClassName="selectedTab"
+                    to="/status"
+                  >
                     {LOCALIZE.mainTabs.statusTabTitle}
-                  </Link>
+                  </NavLink>
                 </li>
               </ul>
               <div id="translation_button" className="translationButton">
@@ -57,10 +97,10 @@ class App extends Component {
             </div>
             <br />
           </nav>
-          <Route exact path="/" component={Home} />
-          <Route path="/experiment" component={Experiment} />
-          <Route path="/status" component={Status} />
-          <Route path="/emib-sample" component={Emib} />
+          <Route exact path={PATH.home} component={Home} />
+          <Route path={PATH.experiment} component={Experiment} />
+          <Route path={PATH.status} component={Status} />
+          <Route path={PATH.emibSampleTest} component={Emib} />
         </div>
       </Router>
     );
