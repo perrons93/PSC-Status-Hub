eba175e9239d8222910b044fa2214ef7e622ca6b
Michael Cherry <zorackiii@gmail.com>
2019-02-20 08:26:04 -0500

    Changing h2 to h1

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -5,7 +5,7 @@ import LOCALIZE from "./text_resources";
 const Home = () => {
   return (
     <div className="app">
-      <h2 id="home-page-paragraph">{LOCALIZE.homePage.title}</h2>
+      <h1 id="home-page-paragraph">{LOCALIZE.homePage.title}</h1>
       <img src={big_welcome_banner} alt={LOCALIZE.homePage.welcomeMsg} />
     </div>
   );
