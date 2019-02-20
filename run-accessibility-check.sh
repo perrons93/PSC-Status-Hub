99fc76d238f9235fd55d1dd965597964a947690f
Francis Normand <francis.normand@canada.ca>
2019-02-20 09:32:14 -0500

    Forgot to uncomment several lines inside accessibility script

--- run-accessibility-check.sh
+++ run-accessibility-check.sh
@@ -4,17 +4,17 @@
 BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 # Build docker (can comment out if already built)
-# docker-compose build
+docker-compose build
 printf "*** ${BLUE}Begin Accessibility Check${NC} ***\n"
-# printf "Home Page Report:\n"
-# pa11y localhost --config ./pa11y.json --include-notices --include-warnings --reporter html > reports/home_page_accessibility_check.html
-# printf "Accessibility reports saved at reports/home_page_accessibility_check.html\n"
-# printf "Prototype Page Report:\n"
-# pa11y localhost/prototype --config ./pa11y.json --include-notices --include-warnings --reporter html > reports/prototype_page_accessibility_check.html
-# printf "Accessibility reports saved at reports/prototype_page_accessibility_check.html\n"
-# printf "Status Page Report:\n"
-# pa11y localhost/status --config ./pa11y.json --include-notices --include-warnings --reporter html > reports/status_page_accessibility_check.html
-# printf "Accessibility reports saved at reports/status_page_accessibility_check.html\n"
+printf "Home Page Report:\n"
+pa11y localhost --config ./pa11y.json --include-notices --include-warnings --reporter html > reports/home_page_accessibility_check.html
+printf "Accessibility reports saved at reports/home_page_accessibility_check.html\n"
+printf "Prototype Page Report:\n"
+pa11y localhost/prototype --config ./pa11y.json --include-notices --include-warnings --reporter html > reports/prototype_page_accessibility_check.html
+printf "Accessibility reports saved at reports/prototype_page_accessibility_check.html\n"
+printf "Status Page Report:\n"
+pa11y localhost/status --config ./pa11y.json --include-notices --include-warnings --reporter html > reports/status_page_accessibility_check.html
+printf "Accessibility reports saved at reports/status_page_accessibility_check.html\n"
 printf "eMIB Sample Test Page Report:\n"
 pa11y localhost/emib-sample --config ./pa11y.json --include-notices --include-warnings --reporter html > reports/emib-sample_page_accessibility_check.html
 printf "Accessibility reports saved at reports/emib-sample_page_accessibility_check.html\n"
