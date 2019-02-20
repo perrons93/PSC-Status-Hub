39e4b31b33318aab65475a9307a53480e6c8a360
Michael Cherry <zorackiii@gmail.com>
2019-02-20 11:30:24 -0500

    Adding stubbed instuctions components, to allow fleshing out of individual components without messy merges.

---
+++ frontend/src/components/eMIB/Evaluation.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Evaluation extends Component {
+  render() {
+    return <div>STUBBED Evaluation Component</div>;
+  }
+}
+
+export default Evaluation;
--- frontend/src/components/eMIB/HowTo.jsx
+++ frontend/src/components/eMIB/HowTo.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
 import LOCALIZE from "../../text_resources";
+import Overview from "./Overview";
+import TipsOnTest from "./TipsOnTest";
+import TestInstructions from "./TestInstructions";
+import Evaluation from "./Evaluation";
 
 class HowTo extends Component {
   render() {
@@ -8,6 +12,10 @@ class HowTo extends Component {
         <h2>{LOCALIZE.emibTest.howToPage.title}</h2>
         <h3>{LOCALIZE.emibTest.howToPage.emailInstructions}</h3>
         <h3>{LOCALIZE.emibTest.howToPage.taskInstructions}</h3>
+        <Overview />
+        <TipsOnTest />
+        <TestInstructions />
+        <Evaluation />
       </div>
     );
   }
