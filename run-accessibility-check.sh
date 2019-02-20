aa4d7403c068db432054c59ee326cd08ce47687c
Francis Normand <francis.normand@canada.ca>
2019-02-20 06:51:43 -0500

    Removed unwanted comment in accessibility script

--- run-accessibility-check.sh
+++ run-accessibility-check.sh
@@ -5,7 +5,6 @@ BLUE='\033[0;34m'
 NC='\033[0m' # No Color
 # Build docker (can comment out if already built)
 docker-compose build
-# Test the backened
 printf "*** ${BLUE}Begin Accessibility Check${NC} ***\n"
 pa11y localhost --config ./pa11y.json --include-notices --include-warnings --reporter html > reports/accessibility_check.html
 printf "Accessibility report saved at reports/accessibility_check.html\n"
