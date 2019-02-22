e2c2e58c34e95826c8581c3d59de1558b601dc33
Francis Normand <francis.normand@canada.ca>
2019-02-22 11:00:54 -0500

    Added a condition that allows the translation of the images in Test Instructions component + Added French images

--- frontend/src/components/eMIB/TestInstructions.jsx
+++ frontend/src/components/eMIB/TestInstructions.jsx
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import "../../css/lib/aurora.min.css";
 import LOCALIZE from "../../text_resources";
 import "../../css/App.css";
+import { LANGUAGES } from "../../components/commons/Translation";
 import original_email_emib_sample_test_example_en from "../../images/original_email_emib_sample_test_example_en.png";
-// import original_email_emib_sample_test_example_fr from "../../images/original_email_emib_sample_test_example_fr.png";
+import original_email_emib_sample_test_example_fr from "../../images/original_email_emib_sample_test_example_fr.png";
 import option_1_emib_sample_test_example_en from "../../images/option_1_emib_sample_test_example_en.png";
-// import option_1_emib_sample_test_example_fr from "../../images/option_1_emib_sample_test_example_fr.png";
+import option_1_emib_sample_test_example_fr from "../../images/option_1_emib_sample_test_example_fr.png";
 import option_2_emib_sample_test_example_en from "../../images/option_2_emib_sample_test_example_en.png";
-// import option_2_emib_sample_test_example_fr from "../../images/option_2_emib_sample_test_example_fr.png";
+import option_2_emib_sample_test_example_fr from "../../images/option_2_emib_sample_test_example_fr.png";
 
 class TestInstructions extends Component {
   render() {
@@ -57,13 +58,23 @@ class TestInstructions extends Component {
             <p className="font-weight-bold underline">
               {LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example.part1Title}
             </p>
-            {/* TODO: (fnormand01) Find a way to put the French image when we click on 'French' button */}
             <p>
-              <img
-                src={original_email_emib_sample_test_example_en}
-                alt={LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example.part1Title}
-                className="test-instruction-image"
-              />
+              {LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example
+                .conditionToDisplayImage === LANGUAGES.english && (
+                <img
+                  src={original_email_emib_sample_test_example_en}
+                  alt={LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example.part1Title}
+                  className="test-instruction-image"
+                />
+              )}
+              {LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example
+                .conditionToDisplayImage === LANGUAGES.french && (
+                <img
+                  src={original_email_emib_sample_test_example_fr}
+                  alt={LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example.part1Title}
+                  className="test-instruction-image"
+                />
+              )}
             </p>
             <p>
               {LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example.part1Description}
@@ -72,21 +83,43 @@ class TestInstructions extends Component {
               {LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example.part2Title}
             </p>
             <p>
-              <img
-                src={option_1_emib_sample_test_example_en}
-                alt={LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example.part2Title}
-                className="test-instruction-image"
-              />
+              {LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example
+                .conditionToDisplayImage === LANGUAGES.english && (
+                <img
+                  src={option_1_emib_sample_test_example_en}
+                  alt={LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example.part2Title}
+                  className="test-instruction-image"
+                />
+              )}
+              {LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example
+                .conditionToDisplayImage === LANGUAGES.french && (
+                <img
+                  src={option_1_emib_sample_test_example_fr}
+                  alt={LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example.part2Title}
+                  className="test-instruction-image"
+                />
+              )}
             </p>
             <p className="font-weight-bold underline">
               {LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example.part3Title}
             </p>
             <p>
-              <img
-                src={option_2_emib_sample_test_example_en}
-                alt={LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example.part3Title}
-                className="test-instruction-image"
-              />
+              {LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example
+                .conditionToDisplayImage === LANGUAGES.english && (
+                <img
+                  src={option_2_emib_sample_test_example_en}
+                  alt={LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example.part3Title}
+                  className="test-instruction-image"
+                />
+              )}
+              {LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example
+                .conditionToDisplayImage === LANGUAGES.french && (
+                <img
+                  src={option_2_emib_sample_test_example_fr}
+                  alt={LOCALIZE.emibTest.howToPage.testInstructions.step2Section.example.part3Title}
+                  className="test-instruction-image"
+                />
+              )}
             </p>
           </div>
         </div>
