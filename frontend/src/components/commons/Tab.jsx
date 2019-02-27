d019609574f1328950a81b656291f0198f4bcc71
Michael Cherry <zorackiii@gmail.com>
2019-02-27 13:32:12 -0500

    Pulled Tab css out of App.css

--- frontend/src/components/commons/Tab.jsx
+++ frontend/src/components/commons/Tab.jsx
@@ -1,6 +1,26 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+const styles = {
+  li: { position: "relative", display: "block", float: "left", marginBottom: "-1px" },
+  button: {
+    position: "relative",
+    display: "block",
+    padding: "10px 15px",
+    backgroundColor: "transparent",
+    marginRight: "2px",
+    lineHeight: "1.42857143",
+    border: "1px solid transparent",
+    borderRadius: "4px 4px 0 0"
+  },
+  active: {
+    color: "black",
+    backgroundColor: "white",
+    border: "1px solid #00565e",
+    borderBottomColor: "white"
+  }
+};
+
 class Tab extends Component {
   static propTypes = {
     tabName: PropTypes.string.isRequired,
@@ -11,15 +31,20 @@ class Tab extends Component {
     return (
       <span>
         {this.props.selected === false && (
-          <li role="presentation">
-            <button id={this.props.tabName} role="tab" aria-selected="false">
+          <li role="presentation" style={styles.li}>
+            <button id={this.props.tabName} style={styles.button} role="tab" aria-selected="false">
               {this.props.tabName}
             </button>
           </li>
         )}
         {this.props.selected === true && (
-          <li role="presentation" className="active">
-            <button id={this.props.tabName} role="tab" aria-selected="true">
+          <li role="presentation" className="active" style={styles.li}>
+            <button
+              id={this.props.tabName}
+              style={{ ...styles.button, ...styles.active }}
+              role="tab"
+              aria-selected="true"
+            >
               {this.props.tabName}
             </button>
           </li>
--- frontend/src/components/commons/TabNavigation.jsx
+++ frontend/src/components/commons/TabNavigation.jsx
@@ -2,6 +2,33 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import Tab from "./Tab";
 
+const styles = {
+  bootstrapNav: {
+    paddingLeft: "0",
+    marginBottom: "0",
+    listStyle: "none",
+    borderBottom: "none"
+  },
+  afterNav: {
+    //this replaces the .bootstrap-tabs > .nav:after
+    content: "",
+    backgroundColor: "white",
+    height: "32px",
+    width: "100%",
+    border: "1px solid #00565e",
+    borderBottomColor: "transparent"
+  },
+  tabContent: {
+    display: "block",
+    WebkitTransition: "opacity 0.15s linear",
+    OTransition: "opacity 0.15s linear",
+    transition: "opacity 0.15s linear",
+    backgroundColor: "white",
+    border: "1px solid #00565e",
+    borderTopColor: "white"
+  }
+};
+
 class TabNavigation extends Component {
   static propTypes = {
     tabSpecs: PropTypes.array.isRequired,
@@ -20,7 +47,7 @@ class TabNavigation extends Component {
   render() {
     return (
       <div className="bootstrap-tabs">
-        <ul role="tablist" className="nav nav-tabs">
+        <ul role="tablist" className="nav nav-tabs" style={styles.bootstrapNav}>
           {this.props.tabSpecs.map((tab, key) => (
             <span key={tab.id} onClick={() => this.selectTab(tab.id)}>
               <Tab tabName={tab.tabName} selected={tab.id === this.state.currentTab} />
@@ -28,7 +55,10 @@ class TabNavigation extends Component {
             </span>
           ))}
         </ul>
-        <div className="tab-content">{this.state.currentBody}</div>
+        <div style={styles.afterNav} />
+        <div style={styles.tabContent} className="tab-content">
+          {this.state.currentBody}
+        </div>
       </div>
     );
   }
