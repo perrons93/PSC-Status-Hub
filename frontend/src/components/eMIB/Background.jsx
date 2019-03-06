dc9bb3e3535c2eca7bcc1b61c3c9df88c75b27a6
Caley Brock <caleybrock7@gmail.com>
2019-03-06 11:49:50 -0500

    Add padding and change text

--- frontend/src/components/eMIB/Background.jsx
+++ frontend/src/components/eMIB/Background.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from "react";
 import LOCALIZE from "../../text_resources";
 
+const styles = {
+  padding: {
+    padding: 20
+  }
+};
+
 class Background extends Component {
   render() {
     return (
-      <div>
+      <div style={styles.padding}>
         <h2>{LOCALIZE.emibTest.backgroundPage.title}</h2>
         <h3>{LOCALIZE.emibTest.backgroundPage.orgChart}</h3>
         <h3>{LOCALIZE.emibTest.backgroundPage.Scenarios}</h3>
