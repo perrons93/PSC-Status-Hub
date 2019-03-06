1e78b767294ae90ce080f6b773e9de09796580e2
Michael Cherry <zorackiii@gmail.com>
2019-03-06 12:07:30 -0500

    Removing unneeded imports

--- frontend/src/components/eMIB/BackgroundInformation.jsx
+++ frontend/src/components/eMIB/BackgroundInformation.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "../../css/lib/aurora.min.css";
 import LOCALIZE from "../../text_resources";
 import "../../css/cat-theme.css";
-import { LANGUAGES } from "../../components/commons/Translation";
 
 class BackgroundInformation extends Component {
   render() {
--- frontend/src/components/eMIB/OrganizationalInformation.jsx
+++ frontend/src/components/eMIB/OrganizationalInformation.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import "../../css/lib/aurora.min.css";
 import LOCALIZE from "../../text_resources";
 import "../../css/cat-theme.css";
-import { LANGUAGES } from "../commons/Translation";
 
 class OrganizationalInformation extends Component {
   render() {
