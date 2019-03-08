6175b34886e899837c04b5c3de767fdc728747a0
Francis Normand <francis.normand@canada.ca>
2019-03-08 08:16:39 -0500

    Merged WarningMessage and ErrorMessage components together + Updated the concerned tests

---
+++ frontend/src/components/commons/SystemMessage.jsx
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+export const MESSAGE_TYPE = {
+  warning: "warning",
+  error: "error"
+};
+
+//only useful for unit test
+export let UNIT_TEST_VALIDATION = {
+  validation: ""
+};
+
+const styles = {
+  logo: {
+    fontSize: "40px"
+  },
+  textBox: {
+    textAlign: "left",
+    paddingLeft: 25
+  }
+};
+
+class WarningMessage extends Component {
+  static propTypes = {
+    messageType: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    message: PropTypes.string.isRequired
+  };
+
+  render() {
+    const { title, message, messageType } = this.props;
+    console.log(messageType);
+    let iconClassName = "";
+    let alertClassName = "";
+    if (messageType === MESSAGE_TYPE.error) {
+      alertClassName = "alert-icon alert-danger";
+      iconClassName = "far fa-times-circle";
+      //only useful for unit test
+      UNIT_TEST_VALIDATION.validation = MESSAGE_TYPE.error;
+    } else {
+      alertClassName = "alert-icon alert-warning";
+      iconClassName = "fas fa-exclamation-circle";
+      //only useful for unit test
+      UNIT_TEST_VALIDATION.validation = MESSAGE_TYPE.warning;
+    }
+
+    return (
+      <div>
+        <div className={alertClassName} role="alert">
+          <div className="icon" aria-hidden="true">
+            <i className={iconClassName} style={styles.logo} />
+          </div>
+          <div style={styles.textBox}>
+            <h3>{title}</h3>
+            <p>{message}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default WarningMessage;
---
+++ frontend/src/tests/components/commons/SystemMessage.test.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { mount } from "enzyme";
+import SystemMessage, {
+  MESSAGE_TYPE,
+  UNIT_TEST_VALIDATION
+} from "../../../components/commons/SystemMessage";
+
+it("renders warning message", () => {
+  const wrapper = mount(
+    <SystemMessage messageType={MESSAGE_TYPE.warning} title={"title"} message={"message"} />
+  );
+  const msgTypeValidation = UNIT_TEST_VALIDATION.validation;
+  const title = <h3>title</h3>;
+  const message = <p>message</p>;
+  expect(msgTypeValidation === MESSAGE_TYPE.warning).toBe(true);
+  expect(wrapper.contains(title)).toEqual(true);
+  expect(wrapper.contains(message)).toEqual(true);
+});
+
+it("renders default message (warning)", () => {
+  const wrapper = mount(
+    <SystemMessage messageType={"hello world"} title={"title"} message={"message"} />
+  );
+  const msgTypeValidation = UNIT_TEST_VALIDATION.validation;
+  const title = <h3>title</h3>;
+  const message = <p>message</p>;
+  expect(msgTypeValidation === MESSAGE_TYPE.warning).toBe(true);
+  expect(wrapper.contains(title)).toEqual(true);
+  expect(wrapper.contains(message)).toEqual(true);
+});
+
+it("renders error message", () => {
+  const wrapper = mount(
+    <SystemMessage messageType={MESSAGE_TYPE.error} title={"title"} message={"message"} />
+  );
+  const msgTypeValidation = UNIT_TEST_VALIDATION.validation;
+  const title = <h3>title</h3>;
+  const message = <p>message</p>;
+  expect(msgTypeValidation === MESSAGE_TYPE.error).toBe(true);
+  expect(wrapper.contains(title)).toEqual(true);
+  expect(wrapper.contains(message)).toEqual(true);
+});
