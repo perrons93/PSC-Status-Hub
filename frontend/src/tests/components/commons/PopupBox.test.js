04f6ca4285e415552ded51c7b357640701333874
Francis Normand <francis.normand@canada.ca>
2019-03-12 07:46:39 -0400

    Fixed the tests since I did some updates

--- frontend/src/tests/components/commons/PopupBox.test.js
+++ frontend/src/tests/components/commons/PopupBox.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
-import PopupBox from "../../../components/commons/PopupBox";
+import PopupBox, { BUTTON_TYPE } from "../../../components/commons/PopupBox";
 
 //also test that it renders without action
 it("renders title, description, left button and right button titles", () => {
@@ -8,7 +8,9 @@ it("renders title, description, left button and right button titles", () => {
     <PopupBox
       title={"title"}
       description={"description"}
+      leftButtonType={BUTTON_TYPE.primary}
       leftButtonTitle={"left button"}
+      rightButtonType={BUTTON_TYPE.secondary}
       rightButtonTitle={"right button"}
     />
   );
@@ -20,7 +22,12 @@ it("renders title, description, left button and right button titles", () => {
 
 it("renders only title, description and left button title", () => {
   const wrapper = shallow(
-    <PopupBox title={"title"} description={"description"} leftButtonTitle={"left button"} />
+    <PopupBox
+      title={"title"}
+      description={"description"}
+      leftButtonType={BUTTON_TYPE.secondary}
+      leftButtonTitle={"left button"}
+    />
   );
   expect(wrapper.find("#unit-test-popup-box-title").exists()).toEqual(true);
   expect(wrapper.find("#unit-test-popup-box-description").exists()).toEqual(true);
@@ -35,8 +42,10 @@ it("calls button actions on click", () => {
     <PopupBox
       title={"title"}
       description={"description"}
+      leftButtonType={BUTTON_TYPE.primary}
       leftButtonTitle={"left button"}
       leftButtonAction={submitMock1}
+      rightButtonType={BUTTON_TYPE.secondary}
       rightButtonTitle={"right button"}
       rightButtonAction={submitMock2}
     />
