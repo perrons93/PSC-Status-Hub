86f7fe54b1c651d546b9237b303b0b28eb49d12c
Francis Normand <francis.normand@canada.ca>
2019-03-18 08:20:03 -0400

    Added zoom functionality to organizational chart (ODC) image

--- frontend/src/components/eMIB/OrganizationalStructure.jsx
+++ frontend/src/components/eMIB/OrganizationalStructure.jsx
@@ -5,7 +5,10 @@ import "../../css/cat-theme.css";
 import { LANGUAGES } from "../commons/Translation";
 import PopupBox, { BUTTON_TYPE } from "../commons/PopupBox";
 import emib_sample_test_example_org_chart_en from "../../images/emib_sample_test_example_org_chart_en.png";
+//TODO (fnormand): Put a zoomed image of better quality
+import emib_sample_test_example_org_chart_en_zoomed from "../../images/emib_sample_test_example_org_chart_en.png";
 import emib_sample_test_example_org_chart_fr from "../../images/emib_sample_test_example_org_chart_fr.png";
+import ImageZoom from "react-medium-image-zoom";
 
 const styles = {
   testImage: {
@@ -73,19 +76,30 @@ class OrganizationalStructure extends Component {
             <p>
               {LOCALIZE.emibTest.howToPage.testExamples.conditionToDisplayImage ===
                 LANGUAGES.english && (
-                <img
-                  src={emib_sample_test_example_org_chart_en}
-                  alt={LOCALIZE.emibTest.background.organizationalStructure.orgChart.desciption}
-                  style={styles.testImage}
+                <ImageZoom
+                  image={{
+                    src: emib_sample_test_example_org_chart_en,
+                    alt: LOCALIZE.emibTest.background.organizationalStructure.orgChart.desciption,
+                    style: styles.testImage
+                  }}
+                  zoomImage={{
+                    src: emib_sample_test_example_org_chart_en_zoomed,
+                    alt: LOCALIZE.emibTest.background.organizationalStructure.orgChart.desciption
+                  }}
                 />
               )}
               {LOCALIZE.emibTest.howToPage.testExamples.conditionToDisplayImage ===
                 LANGUAGES.french && (
-                <img
-                  src={emib_sample_test_example_org_chart_fr}
-                  alt={LOCALIZE.emibTest.background.organizationalStructure.orgChart.desciption}
-                  style={styles.testImage}
-                  longdesc="#org-image-description"
+                <ImageZoom
+                  image={{
+                    src: emib_sample_test_example_org_chart_fr,
+                    alt: LOCALIZE.emibTest.background.organizationalStructure.orgChart.desciption,
+                    style: styles.testImage
+                  }}
+                  zoomImage={{
+                    src: emib_sample_test_example_org_chart_fr,
+                    alt: LOCALIZE.emibTest.background.organizationalStructure.orgChart.desciption
+                  }}
                 />
               )}
             </p>
