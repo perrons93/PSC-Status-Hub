9e3896b6c9096c1e2c226beece358dac36061b6a
Francis Normand <francis.normand@canada.ca>
2019-03-19 11:48:57 -0400

    Undo test updates

--- frontend/src/tests/components/commons/TestFooter.test.js
+++ frontend/src/tests/components/commons/TestFooter.test.js
@@ -6,7 +6,7 @@ it("calls submit test and quit test when the buttons are clicked", () => {
   const submitMock1 = jest.fn();
   const submitMock2 = jest.fn();
   const wrapper = shallow(<TestFooter submitTest={submitMock1} quitTest={submitMock2} />);
-  wrapper.find("#unit-test-submit-btns").simulate("click");
+  wrapper.find("#unit-test-submit-btn").simulate("click");
   wrapper.find("#unit-test-quit-btn").simulate("click");
   expect(submitMock1).toHaveBeenCalledTimes(1);
   expect(submitMock2).toHaveBeenCalledTimes(1);
