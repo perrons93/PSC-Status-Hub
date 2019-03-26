534ba04f2223ea34588ba6e8113413d82b3ece15
Michael Cherry <zorackiii@gmail.com>
2019-03-26 14:11:34 -0400

    Fixing and adding more complex tests

--- frontend/src/tests/components/eMIB/Inbox.test.js
+++ frontend/src/tests/components/eMIB/Inbox.test.js
@@ -1,19 +1,76 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import Inbox from "../../../components/eMIB/Inbox";
+import InboxButton from "../../../components/commons/InboxButton";
+import InboxEmail from "../../../components/commons/InboxEmail";
 import { LANGUAGES } from "../../../components/commons/Translation";
 import LOCALIZE from "../../../text_resources";
+import { getInboxContent } from "../../../components/eMIB/Emib";
 
-it("renders Inbox Page Title in English", () => {
-  LOCALIZE.setLanguage(LANGUAGES.english);
-  const wrapper = shallow(<Inbox />);
-  const inboxPageTitle = <h2>{LOCALIZE.emibTest.inboxPage.title}</h2>;
-  expect(wrapper.contains(inboxPageTitle)).toEqual(true);
+const INBOX_SPECS = getInboxContent();
+
+it("renders Inbox Page", () => {
+  const wrapper = mount(<Inbox inboxSpecs={INBOX_SPECS} />);
+  const emailButton = <InboxButton email={INBOX_SPECS[0]} />;
+  expect(wrapper.contains(emailButton)).toEqual(true);
 });
 
-it("renders Inbox Page Title in French", () => {
-  LOCALIZE.setLanguage(LANGUAGES.french);
-  const wrapper = shallow(<Inbox />);
-  const inboxPageTitle = <h2>{LOCALIZE.emibTest.inboxPage.title}</h2>;
-  expect(wrapper.contains(inboxPageTitle)).toEqual(true);
+it("Shows only email 1", () => {
+  testCore(0);
 });
+
+it("Shows only email 2", () => {
+  testCore(1);
+});
+
+it("Shows only email 3", () => {
+  testCore(2);
+});
+
+it("Shows only email 4", () => {
+  testCore(3);
+});
+
+it("Shows only email 5", () => {
+  testCore(4);
+});
+
+it("Shows only email 6", () => {
+  testCore(5);
+});
+
+it("Shows only email 7", () => {
+  testCore(6);
+});
+
+it("Shows only email 8", () => {
+  testCore(7);
+});
+
+it("Shows only email 9", () => {
+  testCore(8);
+});
+
+it("Shows only email 10", () => {
+  testCore(9);
+});
+
+function testCore(selected) {
+  const wrapper = mount(<Inbox inboxSpecs={INBOX_SPECS} />);
+  wrapper.setState({ currentEmail: selected });
+  for (let i = 0; i < 10; i++) {
+    const currentEmail = INBOX_SPECS[i];
+    const emailButton = <InboxButton email={currentEmail} />;
+    const emailBody = <InboxEmail email={currentEmail} />;
+    expect(wrapper.contains(emailButton)).toEqual(true);
+    const checkBody = wrapper.contains(emailBody);
+    if (i === selected) {
+      // if the current one is selected
+      expect(checkBody).toEqual(true);
+    }
+    if (i !== selected) {
+      // if not
+      expect(checkBody).toEqual(false);
+    }
+  }
+}
