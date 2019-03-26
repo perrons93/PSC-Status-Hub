0e51d5d1879dcaa7871a2312c747987daa2ac742
Michael Cherry <zorackiii@gmail.com>
2019-03-26 12:53:34 -0400

    very generic. displays all data, but needs to be formatted

--- frontend/src/components/eMIB/Inbox.jsx
+++ frontend/src/components/eMIB/Inbox.jsx
@@ -1,23 +1,49 @@
 import React, { Component } from "react";
-import LOCALIZE from "../../text_resources";
-
-const styles = {
-  padding: {
-    padding: 20
-  }
-};
+import PropTypes from "prop-types";
+import InboxButton from "../commons/InboxButton";
+import InboxEmail from "../commons/InboxEmail";
 
 class Inbox extends Component {
+  static propTypes = {
+    inboxSpecs: PropTypes.array.isRequired
+  };
+
+  state = {
+    currentNode: 0
+  };
+
   render() {
+    const length = this.props.inboxSpecs.length - 1;
     return (
-      <div style={styles.padding}>
-        <h2>{LOCALIZE.emibTest.inboxPage.title}</h2>
-        <h3>{LOCALIZE.emibTest.inboxPage.taskList}</h3>
-        <h3>{LOCALIZE.emibTest.inboxPage.notePad}</h3>
-        <h3>{LOCALIZE.emibTest.inboxPage.textTools}</h3>
-        <h3>{LOCALIZE.emibTest.inboxPage.emailFeatures}</h3>
-        <h3>{LOCALIZE.emibTest.inboxPage.progressSaved}</h3>
-        <h3>{LOCALIZE.emibTest.inboxPage.timer}</h3>
+      <div>
+        <nav
+          className="side-nav-grid-buttons-cell"
+          //style={styles.buttonList}
+          role="dialog"
+          aria-label={"Inbox"}
+        >
+          <ul className="nav nav-tabs" /*style={styles.ul}*/ role="menubar">
+            {this.props.inboxSpecs.map(email => (
+              <div key={email.id}>
+                {email.id === this.state.currentNode && (
+                  <li /*style={styles.li}*/ aria-current="page" role="menuitem" />
+                )}
+                {email.id !== this.state.currentNode && (
+                  <li /*style={styles.li}*/ role="menuitem">
+                    <InboxButton text={email.text} />
+                  </li>
+                )}
+              </div>
+            ))}
+          </ul>
+        </nav>
+        <div
+          className="side-nav-grid-content-cell"
+          /*style={styles.bodyContent}*/
+          id="side-nav-grid-content-cell"
+        >
+          <InboxEmail text={this.props.inboxSpecs[this.state.currentNode].body} />
+        </div>
       </div>
     );
   }
