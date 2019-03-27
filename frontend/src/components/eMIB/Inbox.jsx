5b2a63f45cc62d43f61113780e8d33927a34f5bf
Michael Cherry <zorackiii@gmail.com>
2019-03-27 12:42:16 -0400

    Shifting 'side-nav' styles to 'inbox' styles

--- frontend/src/components/eMIB/Inbox.jsx
+++ frontend/src/components/eMIB/Inbox.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import EmailPreview from "./EmailPreview";
 import Email from "./Email";
+import "../../css/inbox.css";
 
 const styles = {
   ul: {
@@ -48,9 +49,9 @@ class Inbox extends Component {
 
   render() {
     return (
-      <div className="side-nav-grid">
+      <div className="inbox-grid">
         <nav
-          className="side-nav-grid-buttons-cell"
+          className="inbox-grid-buttons-cell"
           style={styles.buttonList}
           role="dialog"
           aria-label={"Inbox"}
@@ -69,9 +70,9 @@ class Inbox extends Component {
           </ul>
         </nav>
         <div
-          className="side-nav-grid-content-cell"
+          className="inbox-grid-content-cell"
           style={styles.bodyContent}
-          id="side-nav-grid-content-cell"
+          id="inbox-grid-content-cell"
         >
           <Email email={this.props.inboxSpecs[this.state.currentEmail]} />
         </div>
