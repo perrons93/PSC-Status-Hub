16cbf0a8a39a552e95aa4f7a968192a5e458e603
Michael Cherry <zorackiii@gmail.com>
2019-03-27 10:39:51 -0400

    left alligned text

--- frontend/src/components/eMIB/EmailPreview.jsx
+++ frontend/src/components/eMIB/EmailPreview.jsx
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+/*import email_read from "../../images/email_read.png";
+import email_unread from "../../images/email_unread.png";
+import forward from "../../images/forward.png";
+import reply_all from "../../images/reply_all.png";
+import reply_sent from "../../images/reply_sent.png";
+import reply from "../../images/reply.png";
+import tasks from "../../images/tasks.png";*/
 
 const styles = {
   button: {
@@ -7,7 +14,7 @@ const styles = {
     marginBottom: 10,
     marginLeft: 20,
     marginRight: 20,
-    textAlign: "center",
+    textAlign: "left",
     borderRadius: 4,
     padding: 6,
     border: "2px solid #009fae"
