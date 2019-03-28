9b739cec64db66552f6d3bd640f94a93a1d47844
Michael Cherry <zorackiii@gmail.com>
2019-03-28 14:44:37 -0400

    Using const rather than this.props.email every time

--- frontend/src/components/eMIB/Email.jsx
+++ frontend/src/components/eMIB/Email.jsx
@@ -7,13 +7,14 @@ class Email extends Component {
   };
 
   render() {
+    const email = this.props.email;
     return (
       <span>
-        <div>{this.props.email.subject}</div>
-        <div>{this.props.email.from}</div>
-        <div>{this.props.email.to}</div>
-        <div>{this.props.email.date}</div>
-        <div>{this.props.email.body}</div>
+        <div>{email.subject}</div>
+        <div>{email.from}</div>
+        <div>{email.to}</div>
+        <div>{email.date}</div>
+        <div>{email.body}</div>
       </span>
     );
   }
