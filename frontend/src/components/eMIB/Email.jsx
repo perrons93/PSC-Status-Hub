e5e1f2aa863d7fa241b42400bda0151b678c9f4a
Michael Cherry <zorackiii@gmail.com>
2019-03-28 14:45:53 -0400

    Adding left align on text

--- frontend/src/components/eMIB/Email.jsx
+++ frontend/src/components/eMIB/Email.jsx
@@ -1,6 +1,12 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 
+const styles = {
+  email: {
+    textAlign: "left"
+  }
+};
+
 class Email extends Component {
   static propTypes = {
     email: PropTypes.object.isRequired
@@ -9,13 +15,13 @@ class Email extends Component {
   render() {
     const email = this.props.email;
     return (
-      <span>
+      <div style={styles.email}>
         <div>{email.subject}</div>
         <div>{email.from}</div>
         <div>{email.to}</div>
         <div>{email.date}</div>
         <div>{email.body}</div>
-      </span>
+      </div>
     );
   }
 }
