4a651851f4c713f2a00ed6a876d90a3d72776650
Michael Cherry <zorackiii@gmail.com>
2019-04-01 10:24:16 -0400

    Changed parameters for releaving 'Back to Top'

--- frontend/src/components/commons/BackToTop.jsx
+++ frontend/src/components/commons/BackToTop.jsx
@@ -26,8 +26,15 @@ class BackToTop extends Component {
 
   // handle onScroll event
   handleOnScroll = () => {
+    console.log(window.document.body.offsetHeight);
+    // If the total page is shorter than this size, do not show a back to top button
+    if (window.document.body.offsetHeight < 1800) {
+      this.setState({ buttonVisible: false });
+      return;
+    }
     const currentScroll = document.documentElement.scrollTop;
-    if (currentScroll > 20) {
+    //Only shows up after scrlling for 1000 pxs
+    if (currentScroll > 1000) {
       this.setState({ buttonVisible: true });
     } else {
       this.setState({ buttonVisible: false });
