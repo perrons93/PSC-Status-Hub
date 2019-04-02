9ee301c3deb84fc7202299fbb689d3e42591d948
Michael Cherry <zorackiii@gmail.com>
2019-04-02 14:06:30 -0400

    Fixing description; breaking into two seperate tests

--- frontend/src/tests/modules/TestStatusRedux.test.js
+++ frontend/src/tests/modules/TestStatusRedux.test.js
@@ -4,11 +4,12 @@ import testStatus, {
   deactivateTest
 } from "../../modules/TestStatusRedux";
 
-describe("setLanguage action", () => {
-  it("should update test status to active or inactive", () => {
-    expect(initialState).toEqual({ isTestActive: false });
+describe("test activation state", () => {
+  it("test activate test", () => {
     const action1 = activateTest();
     expect(testStatus(initialState, action1)).toEqual({ isTestActive: true });
+  });
+  it("test deactivate test", () => {
     const action2 = deactivateTest();
     expect(testStatus(initialState, action2)).toEqual({ isTestActive: false });
   });
