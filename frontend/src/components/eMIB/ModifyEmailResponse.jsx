d04bea2718fe1b67915501dfc412e488d872e7d7
Michael Cherry <zorackiii@gmail.com>
2019-04-03 14:49:28 -0400

    Attempt at making a new response with a modal

--- frontend/src/components/eMIB/ModifyEmailResponse.jsx
+++ frontend/src/components/eMIB/ModifyEmailResponse.jsx
@@ -4,6 +4,7 @@ import LOCALIZE from "../../text_resources";
 import PopupBox, { BUTTON_TYPE } from "../commons/PopupBox";
 import ModifyEmailBody from "./ModifyEmailBody";
 import ModifyTaskBody from "./ModifyTaskBody";
+import { Modal } from "react-bootstrap";
 
 export const RESPONSE_TYPE = {
   email: "email",
@@ -17,13 +18,29 @@ export const MODIFICATION_TYPE = {
 
 const styles = {
   title: {
-    top: 0,
-    right: 0,
     backgroundColor: "#00565E",
     color: "white",
     fontSize: 16,
     width: "100%"
-  }
+  } /*,
+  boxshape: {
+    borderWidth: "1px 1px 0 1px",
+    borderStyle: "solid",
+    borderColor: "#00565e",
+    borderRadius: "0px 0px 0px 0px"
+  },
+  boxContent: {
+    paddingLeft: 20,
+    paddingRight: 20
+  }*/,
+  modalHeader: {
+    display: "table",
+    width: "100%"
+  } /*,
+  modelTitle: {
+    position: "absolute",
+    fontSize: "28px"
+  }*/
 };
 
 class ModifyEmailResponse extends Component {
@@ -35,35 +52,51 @@ class ModifyEmailResponse extends Component {
     modificationType: PropTypes.string.isRequired
   };
 
+  buttonCloseAndAction = () => {
+    this.props.saveEmail();
+    this.props.handleClose();
+  };
+
   render() {
     const { showDialog, handleClose, saveEmail, responseType, modificationType } = this.props;
 
     return (
       <div>
-        <PopupBox
-          show={showDialog}
-          handleClose={handleClose}
-          title={
-            <span style={styles.title}>
-              {responseType === RESPONSE_TYPE.email ? (
-                <i className="fas fa-envelope" />
-              ) : (
-                <i className="fas fa-tasks" />
-              )}
-              &emsp;{modificationType === MODIFICATION_TYPE.add ? "TODO Add" : "TODO Modify"}{" "}
-              {responseType === RESPONSE_TYPE.email ? "TODO email" : "TODO task"} response
-            </span>
-          }
-          description={
-            <div>
+        <Modal show={showDialog} onHide={handleClose} style={styles.modal}>
+          <div style={styles.boxContent}>
+            <Modal.Header closeButton style={styles.modalHeader}>
+              <Modal.Title>
+                {
+                  <span style={styles.title}>
+                    {responseType === RESPONSE_TYPE.email ? (
+                      <i className="fas fa-envelope" />
+                    ) : (
+                      <i className="fas fa-tasks" />
+                    )}
+                    &emsp;{modificationType === MODIFICATION_TYPE.add ? "TODO Add" : "TODO Modify"}{" "}
+                    {responseType === RESPONSE_TYPE.email ? "TODO email" : "TODO task"} response
+                  </span>
+                }
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
               {responseType === RESPONSE_TYPE.email ? <ModifyEmailBody /> : <ModifyTaskBody />}
-            </div>
-          }
-          rightButtonType={BUTTON_TYPE.primary}
-          rightButtonTitle={LOCALIZE.commons.startTest}
-          rightButtonAction={saveEmail}
-          noEdge={true}
-        />
+            </Modal.Body>
+            <Modal.Footer>
+              <div>
+                <div>
+                  <button
+                    type="button"
+                    className="btn btn-primary"
+                    onClick={this.buttonCloseAndAction}
+                  >
+                    {LOCALIZE.commons.startTest}
+                  </button>
+                </div>
+              </div>
+            </Modal.Footer>
+          </div>
+        </Modal>
       </div>
     );
   }
