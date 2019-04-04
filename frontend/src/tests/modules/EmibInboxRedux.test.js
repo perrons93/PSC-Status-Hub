2a733d4f84df47ae3fda19b67c4d732f248b98c2
Caley Brock <caleybrock7@gmail.com>
2019-04-04 09:37:46 -0400

    add additional test cases for read emails

--- frontend/src/tests/modules/EmibInboxRedux.test.js
+++ frontend/src/tests/modules/EmibInboxRedux.test.js
@@ -2,27 +2,56 @@ import emibInbox, { initializeEmailSummaries, readEmail } from "../../modules/Em
 import { setLanguage } from "../../modules/LocalizeRedux";
 import { emailsJson } from "../../modules/sampleEmibJson";
 
-const STUBBED_INITIAL_STATE = {
-  emails: emailsJson.emailsEN,
-  emailSummaries: initializeEmailSummaries(emailsJson.emailsEN.length)
-};
-
 describe("setLanguage action", () => {
+  let stubbedInitialState;
+  beforeEach(() => {
+    stubbedInitialState = {
+      emails: emailsJson.emailsEN,
+      emailSummaries: initializeEmailSummaries(emailsJson.emailsEN.length)
+    };
+  });
+
+  afterEach(() => {
+    stubbedInitialState = {};
+  });
   it("should update emails to french or english", () => {
     const action1 = setLanguage("fr");
-    expect(emibInbox(STUBBED_INITIAL_STATE, action1).emails).toEqual(emailsJson.emailsFR);
+    expect(emibInbox(stubbedInitialState, action1).emails).toEqual(emailsJson.emailsFR);
     const action2 = setLanguage("en");
-    expect(emibInbox(STUBBED_INITIAL_STATE, action2).emails).toEqual(emailsJson.emailsEN);
+    expect(emibInbox(stubbedInitialState, action2).emails).toEqual(emailsJson.emailsEN);
   });
 });
 
 describe("read email action", () => {
-  it("should update emails read state to true", () => {
+  let stubbedInitialState;
+  beforeEach(() => {
+    stubbedInitialState = {
+      emails: emailsJson.emailsEN,
+      emailSummaries: initializeEmailSummaries(emailsJson.emailsEN.length)
+    };
+  });
+
+  afterEach(() => {
+    stubbedInitialState = {};
+  });
+
+  it("should update email 0 read state to true", () => {
     const readAction = readEmail(0);
-    expect(emibInbox(STUBBED_INITIAL_STATE, readAction).emailSummaries).toEqual([
-      { isRead: true },
-      { isRead: false },
-      { isRead: false }
-    ]);
+    expect(emibInbox(stubbedInitialState, readAction).emailSummaries[0]).toEqual({
+      isRead: true
+    });
+    expect(emibInbox(stubbedInitialState, readAction).emailSummaries[1]).toEqual({
+      isRead: false
+    });
+  });
+
+  it("should update email 1 read state to true", () => {
+    const readAction = readEmail(1);
+    expect(emibInbox(stubbedInitialState, readAction).emailSummaries[0]).toEqual({
+      isRead: false
+    });
+    expect(emibInbox(stubbedInitialState, readAction).emailSummaries[1]).toEqual({
+      isRead: true
+    });
   });
 });
