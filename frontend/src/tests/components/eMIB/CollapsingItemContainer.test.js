51eb5aef433b11f48a5b52174c9e0e58cbcbb889
Caley Brock <caleybrock7@gmail.com>
2019-04-04 10:20:16 -0400

    Update show body test

--- frontend/src/tests/components/eMIB/CollapsingItemContainer.test.js
+++ frontend/src/tests/components/eMIB/CollapsingItemContainer.test.js
@@ -4,14 +4,13 @@ import CollapsingItemContainer, {
   ICON_TYPE
 } from "../../../components/eMIB/CollapsingItemContainer";
 
-const body = { body: <div>body</div> };
 describe("Icons content types", () => {
   const emailIcon = <i className="fas fa-envelope" />;
   const taskIcon = <i className="fas fa-tasks" />;
 
   it("renders email icon", () => {
     const emailWrapper = shallow(
-      <CollapsingItemContainer iconType={ICON_TYPE.email} title={"title"} body={body} />
+      <CollapsingItemContainer iconType={ICON_TYPE.email} title={"title"} body={<div>body</div>} />
     );
     expect(emailWrapper.containsMatchingElement(emailIcon)).toEqual(true);
     expect(emailWrapper.containsMatchingElement(taskIcon)).toEqual(false);
@@ -28,7 +27,7 @@ describe("Icons content types", () => {
 
 describe("Icons collapsing types", () => {
   const wrapper = shallow(
-    <CollapsingItemContainer iconType={ICON_TYPE.email} title={"title"} body={body} />
+    <CollapsingItemContainer iconType={ICON_TYPE.email} title={"title"} body={<div>body</div>} />
   );
 
   const arrowDownIconDisplayed = <i className="fas fa-angle-down blue-expand-icon" />;
@@ -47,39 +46,25 @@ describe("Icons collapsing types", () => {
   });
 });
 
-/*it("renders title and/or body depending on the 'isCollapsed ' state", () => {
+it("renders title and/or body depending on the 'isCollapsed ' state", () => {
   const wrapper = shallow(
-    <CollapsingItemContainer iconType={ICON_TYPE.email} title={"title"} body={body} />
+    <CollapsingItemContainer iconType={ICON_TYPE.email} title={"title"} body={<div>body</div>} />
   );
-
-  //display only title
-  const contentWhenClosed = (
-    <div>
-      <button>
-        <span />
-        title
-      </button>
-      <span />
-    </div>
+  const titleComponent = (
+    <button>
+      <i />
+      title
+    </button>
   );
-
-  //display title and body
-  const contentWhenExpanded = (
-    <div>
-      <button>
-        <span />
-        title
-      </button>
-      <span />
-      <div>{body}</div>
-    </div>
-  );
+  const bodyComponent = <div>body</div>;
 
   //collapsing item is closed
   wrapper.setState({ isCollapsed: true });
-  expect(wrapper.containsMatchingElement(contentWhenClosed)).toEqual(true);
+  expect(wrapper.containsMatchingElement(titleComponent)).toEqual(true);
+  expect(wrapper.containsMatchingElement(bodyComponent)).toEqual(false);
 
   //collapsing item is expanded
   wrapper.setState({ isCollapsed: false });
-  expect(wrapper.containsMatchingElement(contentWhenExpanded)).toEqual(true);
-});*/
+  expect(wrapper.containsMatchingElement(titleComponent)).toEqual(true);
+  expect(wrapper.containsMatchingElement(bodyComponent)).toEqual(true);
+});
