13d6a024b2e5a6562a534f17eca5a166a232eabc
Caley Brock <caleybrock7@gmail.com>
2019-04-05 18:50:44 -0400

    Add email test

--- frontend/src/tests/components/eMIB/Email.test.js
+++ frontend/src/tests/components/eMIB/Email.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
-import Email from "../../../components/eMIB/Email";
+import { UnconnectedEmail as Email } from "../../../components/eMIB/Email";
+import CollapsingItemContainer from "../../../components/eMIB/CollapsingItemContainer";
 
 const emailStub = {
   id: 1,
@@ -14,22 +15,40 @@ const emailStub = {
 const hasAction = <i className="fas fa-sign-out-alt" style={{ color: "#00565E" }} />;
 
 it("default email renders with subject as an h3", () => {
-  const wrapper = shallow(<Email email={emailStub} emailCount={0} taskCount={0} />);
+  const wrapper = shallow(
+    <Email email={emailStub} emailCount={0} taskCount={0} emailActions={[]} />
+  );
   const subject = <h3>Subject 1</h3>;
   expect(wrapper.contains(subject)).toEqual(true);
   expect(wrapper.contains(hasAction)).toEqual(false);
 });
 
 it("shows action when email count is non zero", () => {
-  const wrapper = shallow(<Email email={emailStub} emailCount={1} taskCount={0} />);
+  const wrapper = shallow(
+    <Email email={emailStub} emailCount={1} taskCount={0} emailActions={[]} />
+  );
   const subject = <h3>Subject 1</h3>;
   expect(wrapper.contains(subject)).toEqual(true);
   expect(wrapper.contains(hasAction)).toEqual(true);
 });
 
 it("shows action when task count is non zero", () => {
-  const wrapper = shallow(<Email email={emailStub} emailCount={0} taskCount={2} />);
+  const wrapper = shallow(
+    <Email email={emailStub} emailCount={0} taskCount={2} emailActions={[]} />
+  );
   const subject = <h3>Subject 1</h3>;
   expect(wrapper.contains(subject)).toEqual(true);
   expect(wrapper.contains(hasAction)).toEqual(true);
+});
+
+it("shows as many CollapsingItemContainer as there are actions", () => {
+  const wrapper = shallow(
+    <Email
+      email={emailStub}
+      emailCount={0}
+      taskCount={2}
+      emailActions={[{ emailType: "reply", emailTo: "you", emailBody: "hi" }]}
+    />
+  );
+  expect(wrapper.find(CollapsingItemContainer).length).toEqual(1);
 });
