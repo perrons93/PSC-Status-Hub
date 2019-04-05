c4d0784db05ba8eb19598330cd5c0f125817a04e
Caley Brock <caleybrock7@gmail.com>
2019-04-05 10:53:37 -0400

    Update Email component unit tests

--- frontend/src/tests/components/eMIB/Inbox.test.js
+++ frontend/src/tests/components/eMIB/Inbox.test.js
@@ -2,22 +2,12 @@ import React from "react";
 import { shallow } from "enzyme";
 import { UnconnectedInbox as Inbox } from "../../../components/eMIB/Inbox";
 import { emailsJson } from "../../../modules/sampleEmibJson";
+import EmailPreview from "../../../components/eMIB/EmailPreview";
+import Email from "../../../components/eMIB/Email";
 
 const INBOX_SPECS = emailsJson.emailsEN;
 
-it("Shows only email 1", () => {
-  testCore(0);
-});
-
-it("Shows only email 2", () => {
-  testCore(1);
-});
-
-it("Shows only email 3", () => {
-  testCore(2);
-});
-
-function testCore(selected) {
+it("Displays 3 email previews when there are 3 emails", () => {
   const wrapper = shallow(
     <Inbox
       emails={INBOX_SPECS}
@@ -29,17 +19,6 @@ function testCore(selected) {
       readEmail={() => {}}
     />
   );
-  wrapper.setState({ currentEmail: selected });
-  for (let i = 0; i < 3; i++) {
-    const currentEmail = INBOX_SPECS[i];
-    const subjectHeader = <h3>{currentEmail.subject}</h3>;
-    const checkBody = wrapper.contains(subjectHeader);
-    if (i === selected) {
-      // if the current email is selected, the body should be shown
-      expect(checkBody).toEqual(true);
-    }
-    if (i !== selected) {
-      expect(checkBody).toEqual(false);
-    }
-  }
-}
+  expect(wrapper.find(EmailPreview).length).toEqual(INBOX_SPECS.length);
+  expect(wrapper.find(Email).length).toEqual(1);
+});
