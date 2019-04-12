e82b07be1be07c7b22a743db98b29d648efa60e1
Michael Cherry <zorackiii@gmail.com>
2019-04-12 09:05:56 -0400

    Adding tests to verify that the proper number of <p> tags are generated in the email body

--- frontend/src/tests/components/eMIB/Email.test.js
+++ frontend/src/tests/components/eMIB/Email.test.js
@@ -96,3 +96,36 @@ describe("shows as many 'CollapsingItemContainer' as there are actions", () => {
     expect(wrapper.find(CollapsingItemContainer).length).toEqual(3);
   });
 });
+
+describe("email body renders the proper number of <p> tags in the body", () => {
+  it("renders only 1 <p> with a short body", () => {
+    testParagraphTags("I am a one line email", 1);
+  });
+
+  it("renders only 2 <p>'s with a single \n body", () => {
+    testParagraphTags("I am a one line\nI am the next", 2);
+  });
+
+  it("renders only 2 <p>'s with a body split by \n\n", () => {
+    testParagraphTags("I am a one line\n\nI am the next", 2);
+  });
+
+  it("renders only 4 <p>'s despite varying  \n's", () => {
+    testParagraphTags("Dear Joe\nHow are you?\n\n\n\n\n\n\nSincerely,\n\nBob", 4);
+  });
+
+  function testParagraphTags(body, expectedCount) {
+    const emailStub = {
+      id: 0,
+      to: "To 1",
+      from: "From 1",
+      subject: "Subject 1",
+      date: "Date 1",
+      body: body
+    };
+    const wrapper = shallow(
+      <Email email={emailStub} emailCount={0} taskCount={2} emailActionsArray={[[]]} />
+    );
+    expect(wrapper.find("p").length).toEqual(expectedCount);
+  }
+});
