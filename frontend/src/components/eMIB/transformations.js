3957383c50b1d33dd2fbde662dab375cba4f5f20
Michael Cherry <zorackiii@gmail.com>
2019-04-19 13:22:10 -0400

    creating transformations scripts

---
+++ frontend/src/components/eMIB/transformations.js
@@ -0,0 +1,13 @@
+// transform address book into list of options
+export function transformAddressBook(addressBook) {
+  let options = [{ text: "None", value: "null" }];
+  for (let contact of addressBook) {
+    options.push(transformContact(contact));
+  }
+  return options;
+}
+
+// transform contact shape into options shape
+export function transformContact(contact) {
+  return { text: contact.name + "(" + contact.role + ")", value: contact.id };
+}
