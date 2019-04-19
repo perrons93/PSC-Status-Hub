fb3d8c4ab9db4e6055495f4f3ca4befce3e6611b
Michael Cherry <zorackiii@gmail.com>
2019-04-19 10:51:41 -0400

    Moving styles from constants.js to styleConstants.js

---
+++ frontend/src/components/eMIB/styleConstants.js
@@ -0,0 +1,18 @@
+export const styleConstants = {
+  instuctions: {
+    h2: {
+      margin: 0
+    },
+    h3: {
+      marginTop: 32,
+      marginBottom: 0
+    },
+    h4: {
+      marginTop: 24,
+      marginBottom: 0
+    },
+    p: {
+      margin: 0
+    }
+  }
+};
