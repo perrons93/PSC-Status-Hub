86a746a0445de26b3c0742fd3f8de0c1f7d5affb
Francis Normand <francis.normand@canada.ca>
2019-04-24 14:00:18 -0400

    Added comments in 'editActionDialogHelper.js' file

--- frontend/src/helpers/editActionDialogHelper.js
+++ frontend/src/helpers/editActionDialogHelper.js
@@ -1,3 +1,5 @@
+/* verify if the email form is empty by checking if all the fields are empty
+returns true if all the fields are empty and false if at least one of the fields contains data */
 function isEmailFormEmpty(emailType, emailTo, emailCc, emailResponse, reasonsForActionContent) {
   let isEmpty = false;
 
@@ -15,6 +17,8 @@ function isEmailFormEmpty(emailType, emailTo, emailCc, emailResponse, reasonsFor
   return isEmpty;
 }
 
+/* verify if the task form is empty by checking if all the fields are empty
+returns true if all the fields are empty and false if at least one of the fields contains data */
 function isTaskFormEmpty(taskContent, reasonsForActionContent) {
   let isEmpty = false;
 
@@ -26,6 +30,10 @@ function isTaskFormEmpty(taskContent, reasonsForActionContent) {
   return isEmpty;
 }
 
+/* verify if the task form has been edited by comparing the initial field values (getting these values
+from the action prop) with the current field values (getting these values from the action state)
+returns false if all initial fields values are the same as the current ones and true if at least
+one of the initial field values is diferrent as the current ones */
 function isTaskFormEdited(
   initialTaskContent,
   taskContent,
@@ -45,6 +53,10 @@ function isTaskFormEdited(
   return isEdited;
 }
 
+/* verify if the email form has been edited by comparing the initial field values (getting these values
+from the action prop) with the current field values (getting these values from the action state)
+returns false if all initial fields values are the same as the current ones and true if at least
+one of the initial field values is diferrent as the current ones */
 function isEmailFormEdited(
   initialEmailType,
   emailType,
